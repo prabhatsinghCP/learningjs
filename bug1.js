474a607b5ca0a85768274ea034bf52a8c26d5f8d
Prabhat Singh <singhgprabhat1980@gmail.com>
2025-01-11 20:37:00 +0530

    changes made in bug1.js in main

--- bug1.js
+++ bug1.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
-This is my BigUint64Array
+This is my conflict merging 
 
-I eanted to keep this bug with the task
-=======
 2025 is about to begin 
 
-I hate easy things 
-
 I love challenges
->>>>>>> bug-fix
+
+
+
+I am adding it just to understand git diff Command 
